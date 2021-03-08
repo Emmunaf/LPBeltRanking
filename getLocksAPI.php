87b7495d3c9f7f840841cb41ceaec3f8352330cc
Emanuele <ema.muna95@gmail.com>
2021-03-08 19:17:41 +0100

    Added basic API

---
+++ getLocksAPI.php
@@ -0,0 +1,88 @@
+<?php
+/*
+    API used to get locs info.
+*/
+header('Content-Type: application/json; charset=UTF-8');
+require_once("./site_config.php");
+
+function filter_lock_info($lockInfo){
+    $validation = "/^(http|https):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i";
+
+    
+    $lockInfo["Model"] = htmlspecialchars($lockInfo["Model"], ENT_QUOTES| ENT_HTML401, 'UTF-8');
+    
+    if(!$lockInfo["ImageURL"] ){ // fallback to default generic
+		$lockInfo["ImageURL"] ="https://cdn.pixabay.com/photo/2012/04/01/18/59/padlock-24051_960_720.png";
+	}
+    $lockInfo["ImageURL"] = htmlspecialchars($lockInfo["ImageURL"], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+    
+    $lockInfo["NumPin"] = htmlspecialchars($lockInfo["NumPin"], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+    $lockInfo["Manufacturer"] = htmlspecialchars($lockInfo["Manufacturer"], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+    $lockInfo["Country"] = htmlspecialchars($lockInfo["Country"], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+    if(!$lockInfo["Price"] ){ // fallback to default generic
+		$lockInfo["Price"] = "N.A.";
+	}
+    $lockInfo["Price"] = htmlspecialchars_decode($lockInfo["Price"], ENT_QUOTES);
+    $lockInfo["Note"] = htmlspecialchars($lockInfo["Note"], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+    $lockInfo["Belt"] = htmlspecialchars($lockInfo["Belt"], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+    $lockInfo["LockwikiURL"] = htmlspecialchars($lockInfo["LockwikiURL"], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+
+    return $lockInfo;
+}
+function filter_lock_infos($locksInfo){
+    $new_arr = array();
+    foreach($locksInfo as $key => $lockInfo) {
+        array_push($new_arr, filter_lock_info($lockInfo));
+    }
+    return $new_arr;
+}
+
+function connectMySQL() {
+	$mysqli = new mysqli(DBAddr, DBUser, DBPassword, DBName);
+	/* check connection */
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_errno);
+		exit();
+	}
+	
+	
+	return $mysqli;
+}
+
+function get_locks(){
+		$locksInfo = array();
+		$conn = connectMySql();
+		if(!$conn){
+			printf("Connect failed: %s\n", $mysqli->connect_errno);
+		}
+		$conn->query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
+
+		$stmt = $conn->prepare("SELECT *
+								FROM
+									locks
+								WHERE
+									1
+								");//ORDER BY D;
+	
+		if (!$stmt) {
+			printf("Error message: %s\n", $conn->error);
+			return false;
+		}
+		//$stmt->bind_param("d",);
+		$stmt->execute();
+		$res = $stmt->get_result();
+		if (!$res) {
+			return false;
+		}
+		while ($row = $res->fetch_assoc()) {
+			array_push($locksInfo, $row);
+		}
+		return $locksInfo;
+	}
+
+// Encode as json, add the status field and send it through the net!
+
+$locksInfo = get_locks();
+$locksInfo = filter_lock_infos($locksInfo);
+echo json_encode(array('status' => 'ok','message'=>$locksInfo));
+
---
+++ site_config.php
@@ -0,0 +1,12 @@
+<?php
+/*This is a PHP Config file */
+define('DBName', 'lockpicking');
+define('DBUser', 'root');
+define('DBPassword', '');
+define('DBAddr', 'localhost');
+define('TITLE', 'BeltRank');
+//define('PLATFORM_PATH', 'https://localhost/');
+//define('FULL_PATH', $_SERVER['DOCUMENT_ROOT']."//");
+//define("MAIL_DISABLE", TRUE);
+error_reporting(E_ALL & ~E_NOTICE);
+?>
